53fd1150c428241289b55fe2c05e09efc07a0164
Alejandro <awscodeapp@gmail.com>
2026-01-20 16:40:20 -0600

    App Name changed to Cosulta Experto

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'ConsultaExpertoApp',
+  appName: 'Consulta Experto',
   webDir: 'dist',
   server: {
     androidScheme: 'https',
