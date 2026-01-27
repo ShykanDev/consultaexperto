c37a01207a1ea9abdb4513d092ae3db21435e049
Alejandro <awscodeapp@gmail.com>
2026-01-27 11:49:34 -0600

    Updates

--- assets/expertAdmin-VhPRsj3U.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./index-9KC0srZ2.js";const a=e("expertAdmin",{state:()=>({expertData:{}}),getters:{getCurrentExpert:t=>t.expertData},actions:{setExpertData(t){this.expertData=t},resetCurrentExpert(){this.expertData={}}},persist:!0});export{a as u};
