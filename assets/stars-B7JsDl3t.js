ad79eed0a7dd3b652d02d4be31a9f788ca56225b
Alejandro <awscodeapp@gmail.com>
2026-01-27 11:35:00 -0600

    Updates

---
+++ assets/stars-B7JsDl3t.js
@@ -0,0 +1,1 @@
+const n=()=>({calcStarsValue:r=>{const{total:t,count:a}=r;return!a||!t?0:t/a}});export{n as u};
---
+++ assets/stars-legacy-um4h9HOd.js
@@ -0,0 +1,1 @@
+System.register([],function(t,e){"use strict";return{execute:function(){t("u",()=>({calcStarsValue:t=>{const{total:e,count:u}=t;return u&&e?e/u:0}}))}}});
